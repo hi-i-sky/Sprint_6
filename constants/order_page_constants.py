52c5efe69bc17655b2599b84530b2f4ac5056d09
hi-i-sky <okesan@yandex.ru>
2026-09-03 17:30:55 +0300

    убрала условие и параметризацию в тесте - сделала два отдельных метода

--- constants/order_page_constants.py
+++ constants/order_page_constants.py
@@ -1,36 +1,15 @@
-from locators.order_page_locators import (
-    DATE_9_BUTTON,
-    DATE_10_BUTTON,
-    TWO_DAYS_PERIOD_BUTTON,
-    THREE_DAYS_PERIOD_BUTTON,
-    CHECKBOX_BLACK,
-    CHECKBOX_GREY,
-)
+TOP_NAME = 'Анастасия'
+TOP_SURNAME = 'Сафронова'
+TOP_ADDRESS = 'Госпитальный переулок, д. 4, стр. 1'
+TOP_STATION = 'Бауманская'
+TOP_PHONE = '+79990000000'
+TOP_COMMENT = 'Проходная в общежитии, позвоните, как подойдете'
 
-FIRST_TEST_DATA = {
-    "entry_type": "top",
-    "name": "Анастасия",
-    "surname": "Сафронова",
-    "address": "Госпитальный переулок, д. 4, стр. 1",
-    "station": "Бауманская",
-    "phone": "+79990000000",
-    "comment": "Проходная в общежитии, позвоните, как подойдете",
-    "date": DATE_9_BUTTON,
-    "period": TWO_DAYS_PERIOD_BUTTON,
-    "color": CHECKBOX_BLACK
-}
-
-SECOND_TEST_DATA ={
-    "entry_type": "bottom",
-    "name": "Александр",
-    "surname": "Абраменко",
-    "address": "пр. Ленина, 1",
-    "station": "Сокольники",
-    "phone": "+79991111111",
-    "comment": "Не опаздывайте, пожалуйста",
-    "date": DATE_10_BUTTON,
-    "period": THREE_DAYS_PERIOD_BUTTON,
-    "color": CHECKBOX_GREY
-}
+BOTTOM_NAME = 'Александр'
+BOTTOM_SURNAME = 'Абраменко'
+BOTTOM_ADDRESS = 'пр. Ленина, 1'
+BOTTOM_STATION = 'Сокольники'
+BOTTOM_PHONE = '+79991111111'
+BOTTOM_COMMENT = 'Не опаздывайте, пожалуйста'
 
 EXPECTED_SUCCESS_TEXT = 'Заказ оформлен'
--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,39 +1,37 @@
-import pytest
 import allure
 from pages.order_page import OrderPage
-from constants.order_page_constants import FIRST_TEST_DATA, SECOND_TEST_DATA, EXPECTED_SUCCESS_TEXT
+from constants.order_page_constants import TOP_NAME, TOP_SURNAME, TOP_ADDRESS, TOP_STATION, TOP_PHONE, TOP_COMMENT, BOTTOM_NAME, BOTTOM_SURNAME, BOTTOM_ADDRESS, BOTTOM_STATION, BOTTOM_PHONE, BOTTOM_COMMENT, EXPECTED_SUCCESS_TEXT
+from locators.order_page_locators import DATE_10_BUTTON, THREE_DAYS_PERIOD_BUTTON, CHECKBOX_GREY, DATE_9_BUTTON, TWO_DAYS_PERIOD_BUTTON, CHECKBOX_BLACK
 
 
 class TestOrderPage:
 
-    @allure.title('Проверяем флоу позитивного сценария заказа самоката')
-    @pytest.mark.parametrize('test_data', [FIRST_TEST_DATA, SECOND_TEST_DATA])
-    def test_order_scooter_success(self, driver, test_data):
-
+    @allure.title('Проверяем флоу позитивного сценария заказа самоката через кнопку наверху страницы')
+    def test_order_scooter_by_top_order_button_success(self, driver):
         order_page = OrderPage(driver)
         order_page.get_main_page()
-
-        if test_data["entry_type"] == "top":
-            order_page.click_top_order_button()
-        elif test_data["entry_type"] == "bottom":
-            order_page.click_bottom_order_button()
-
+        order_page.click_top_order_button()
         order_page.wait_for_load_first_order_page()
 
-        order_page.set_first_page_order(
-        name=test_data["name"],
-        surname=test_data["surname"],
-        address=test_data["address"],
-        station=test_data["station"],
-        phone=test_data["phone"]
-        )
+        order_page.set_first_page_order(TOP_NAME, TOP_SURNAME, TOP_ADDRESS, TOP_STATION, TOP_PHONE)
+        order_page.set_second_page_order(DATE_9_BUTTON, TWO_DAYS_PERIOD_BUTTON, CHECKBOX_BLACK, TOP_COMMENT)
+        
+        order_page.wait_for_load_confirm_window()
+        order_page.confirm_order()
+        order_page.wait_for_load_success_window()
 
-        order_page.set_second_page_order(
-        date_locator=test_data["date"],
-        period_locator=test_data["period"],
-        color_locator=test_data["color"],
-        comment=test_data["comment"]
-        )
+        assert EXPECTED_SUCCESS_TEXT in order_page.get_success_text()
+
+
+    @allure.title('Проверяем флоу позитивного сценария заказа самоката через кнопку внизу страницы')
+    def test_order_scooter_by_bottom_order_button_success(self, driver):
+        order_page = OrderPage(driver)
+        order_page.get_main_page()
+        order_page.click_bottom_order_button()
+        order_page.wait_for_load_first_order_page()
+
+        order_page.set_first_page_order(BOTTOM_NAME, BOTTOM_SURNAME, BOTTOM_ADDRESS, BOTTOM_STATION, BOTTOM_PHONE)
+        order_page.set_second_page_order(DATE_10_BUTTON, THREE_DAYS_PERIOD_BUTTON, CHECKBOX_GREY, BOTTOM_COMMENT)
         
         order_page.wait_for_load_confirm_window()
         order_page.confirm_order()
@@ -44,7 +42,6 @@ class TestOrderPage:
 
     @allure.title('Проверяем кнопку с логотипом Яндекса на главной странице')
     def test_ya_button(self, driver):
-
         order_page = OrderPage(driver)
         order_page.get_main_page()
         order_page.click_ya_button()
@@ -53,7 +50,6 @@ class TestOrderPage:
 
     @allure.title('Проверяем кнопку с логотипом Самоката на главной странице')
     def test_home_button(self, driver):
-
         order_page = OrderPage(driver)
         order_page.get_main_page()
         order_page.click_home_button()
